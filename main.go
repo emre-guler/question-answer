3a6409312671a110b936d687be54df2b57610b9c
emre-guler <emre_guler01@hotmail.com>
2022-08-10 02:41:13 +0300

    progress

--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"net/http"
 	"os"
+	"text/template"
 )
+
+var loginTemp = template.Must(template.ParseFiles("./view/login.html"))
 
 func main() {
 	port := os.Getenv("PORT")
@@ -17,5 +20,10 @@ func main() {
 }
 
 func loginHandler(w http.ResponseWriter, r *http.Request) {
-
+	switch r.Method {
+	case "GET":
+	case "POST":
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
 }
