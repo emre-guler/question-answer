1e16fdcf2f83bab8c38f7aa5266e068218664c4e
emre-guler <emre_guler01@hotmail.com>
2022-08-10 23:04:10 +0300

    progress

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/emre-guler/question-answer
+
+go 1.18
--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 	"text/template"
 )
 
-var loginTemp = template.Must(template.ParseFiles("./view/login.html"))
+var loginTemp = template.Must(template.ParseFiles("./views/login.html"))
 
 func main() {
 	port := os.Getenv("PORT")
@@ -19,11 +19,14 @@ func main() {
 	http.ListenAndServe((":" + port), mux)
 }
 
+// TODO Login with X platform
 func loginHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "GET":
+		loginTemp.Execute(w, nil)
 	case "POST":
 	default:
 		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.Write([]byte("405: Method not allowed."))
 	}
 }
